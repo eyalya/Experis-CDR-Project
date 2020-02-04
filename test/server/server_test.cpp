9c4f60e929a934581627de56a709c7fc82a42701
h60.eyal <eyalyaish@gmail.com>
2020-02-04 20:30:09 +0200

    server test changed to mutest

--- test/server/server_test.cpp
+++ test/server/server_test.cpp
@@ -1,3 +1,4 @@
+#include "mu_test.h"
 #include <iostream>
 
 #include "TCP_acceptor.hpp"
@@ -26,9 +27,50 @@ typedef advcpp::Upsertors< char* > HashUpsertors;
 
 static const char* LOOPBACK_ADDR = "127.0.0.1";
 static const int port = 2300;
+UNIT(server_one_thread_each)
+    bool switchButton = true;
+    const size_t nRecivers = 1;
+    const size_t nUpsertors = 1;
 
-int main()
-{    
+    WQSocket queueSocket;
+    WQMsg msgQue(10000);
+
+    HashConts dsCont;
+
+    std::vector<advcpp::Irecievers*> msgRecieversVec;
+    advcpp::ReciversCreate (msgRecieversVec, queueSocket, msgQue, switchButton, nRecivers);
+
+    std::vector<advcpp::IReducing* > reducingVec;
+    reducingVec.push_back (new advcpp::MCOReducing()); 
+    reducingVec.push_back (new advcpp::MCOReducing());
+    advcpp::Reducer reducer(reducingVec);
+
+    std::vector<advcpp::IDecodeMassge* > decodersVec;
+    decodersVec.push_back(new advcpp::DecodeMCO());
+    decodersVec.push_back(new advcpp::DecodeMCO());
+    advcpp::CdrDecoder decoder(decodersVec);
+
+    advcpp::RecordAggregator<char*> recorder(decoder ,reducer);
+
+    std::vector<HashUpsertors* > upsertorsVec;
+    
+    advcpp::UpsertorsCreate<char*>(upsertorsVec, msgQue, recorder, dsCont, switchButton, nUpsertors);
+
+    advcpp::Dispatcher<advcpp::Irecievers*> dispatcher(msgRecieversVec, switchButton);
+    dispatcher.ActivateWorkers();
+
+    advcpp::CdrFactory<HashUpsertors*> cdrFactory(upsertorsVec, switchButton);
+    cdrFactory.ActivateWorkers();
+
+    advcpp::TCPAcceptor acceptor(LOOPBACK_ADDR, port, queueSocket);
+    advcpp::AcceptorThread acceptorActivator(&acceptor);
+    advcpp::Thread acceptorThread(&acceptorActivator);
+
+    acceptorThread.Join();
+    ASSERT_PASS();
+END_UNIT
+
+UNIT(server_multi)
     bool switchButton = true;
     const size_t nRecivers = advcpp::NT - 1;
     const size_t nUpsertors = 2;
@@ -68,7 +110,12 @@ int main()
     advcpp::Thread acceptorThread(&acceptorActivator);
 
     acceptorThread.Join();
-    return 0;
-}
+    ASSERT_PASS();
+END_UNIT
 
+
+TEST_SUITE(Collect knowledge from every place you can)
+    TEST(server_multi)
+    TEST(server_one_thread_each)
+END_SUITE
 
